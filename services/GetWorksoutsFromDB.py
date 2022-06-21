dce9c8003fbd6ff33b4406e405865701ccdcd0aa
Marissa <marissacavanaugh123@gmail.com>
2022-06-21 10:42:40 -0400

    Adding initial python services

---
+++ services/GetWorksoutsFromDB.py
@@ -0,0 +1,55 @@
+import psycopg2
+
+
+
+def getCurConn():
+    postgresHost = "127.0.0.1"
+    postgresDBName = "Workout"
+    postgresUser = "postgres"
+    postgrespw = "Theroo52!"
+    conn = psycopg2.connect(
+        host=postgresHost,
+        database=postgresDBName,
+        user=postgresUser,
+        password=postgrespw)
+    cur = conn.cursor()
+    return cur, conn
+
+
+
+def getDistinct(colName, tableName):
+    cur, conn = getCurConn()
+    try:
+        cur.execute("select distinct \"" + colName + "\" from " + tableName)
+        result = cur.fetchall()
+        listOfVals = [i[0] for i in result]
+        cur.close()
+        conn.close()
+        return listOfVals
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(error)
+        cur.execute("ROLLBACK")
+        conn.commit()
+        return "Error"
+
+
+def queryDBWhere(tableName, colName, workoutName):
+    cur, conn = getCurConn()
+    try:
+        cur.execute("select * from " + tableName + " where \"" + colName + "\"=%s", [workoutName])
+        colnames = [desc[0] for desc in cur.description]
+        result = cur.fetchall()
+        listOfResults = []
+        for i in result:
+            rowList = []
+            for j in range(len(colnames)):
+                rowList.append(i[j])
+            listOfResults.append(rowList)
+        return listOfResults
+
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(error)
+        cur.execute("ROLLBACK")
+        conn.commit()
+        return "Error"
